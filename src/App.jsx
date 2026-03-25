fda84f6574da4435fd165af3cd636c110b900cff
matchmymakeup <commsafe@proton.me>
2026-03-25 23:38:18 +1000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import MatchResults from './pages/MatchResults'
 import ColorScanner from './pages/ColorScanner'
+import Library from './pages/Library'
+import Terms from './pages/Terms'
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
-        <Route path="/" element={<Navigate to="/Home" replace />} />
-        <Route path="/Home" element={<Home />} />
-        <Route path="/MatchResults" element={<MatchResults />} />
+        <Route path="/" element={<Home />} />
         <Route path="/ColorScanner" element={<ColorScanner />} />
-        <Route path="*" element={<Navigate to="/Home" replace />} />
+        <Route path="/MatchResults" element={<MatchResults />} />
+        <Route path="/Library" element={<Library />} />
+        <Route path="/Terms" element={<Terms />} />
+        <Route path="*" element={<Home />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
